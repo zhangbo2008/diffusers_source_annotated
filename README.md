cc50319d65a3681a4725f6df59d155c85cc27d39
zhangbo2008 <15122306087@163.com>
2023-06-15 09:03:22 +0800

    no_comment

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # diffusers_source_annotated
+
+
+
+读源码, 了解diffusers里面的调用细节和底层原理.
+最好先把doc看了
+
+
+
+
+读一下diffusion文档:对于difusion的理解还是非常差!!!!!!!!!	 
+	https://huggingface.co/docs/diffusers/index
--- models/attention_flax.py
+++ models/attention_flax.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# 这里面代码 有的torch实现有的flax实现.所以2个版本.
 import flax.linen as nn
 import jax.numpy as jnp
 
